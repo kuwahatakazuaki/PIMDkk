64d95427d93ca2ad0e38cf7d3e0455ba055b397f
kuwahatakazuaki <kuwahata0615@gmail.com>
2024-06-15 18:43:50 +0900

    BugFix set_nnp_araidai in neural_network.F90

--- neural_network.F90
+++ neural_network.F90
@@ -20,46 +20,71 @@ subroutine force_nnp_araidai
   use Parameters, &
     only: Eenergy, r, fr, Natom, AUtoAng, eVtoAU, dp_inv, alabel, &
           addresstmp, Ista, Iend, laddress
-  implicit none
+  use utility, only: program_abort
 
-  character(Len=130) :: line
-  integer :: imode, i, Uout, Uinp
+  implicit none
+  character(Len=130) :: line, inp_train(8)
+  integer :: imode, i, Uout, Uinp, ios
   character :: dummyC
-  !real(8), parameter :: eVtoAU  = 1.0d0 / 27.21138505
   real(8), parameter :: eVAng_HartBohr = 0.5291772108d0 / 27.21138505d0
+
+  open(newunit=Uinp,file='training.data_1',status='old',iostat=ios)
+    if ( ios /= 0) then
+      call program_abort('ERROR!!: There is no file of "training.data_1"')
+    end if
+    do i = 1, 5
+      read(Uinp,'(a)') inp_train(i)
+    end do
+    do i = 1, Natom
+      read(Uinp,'()')
+    end do
+    do i = 6, 8
+      read(Uinp,'(a)') inp_train(i)
+    end do
+  close(Uinp)
+
+!do i = 1, 8
+!  print *, inp_train(i)
+!end do
 
   do imode = Ista, Iend
     write(addresstmp(laddress+1:laddress+6),'(i5.5,A1)') imode,'/'
 
-    open(newunit=Uout,file=trim(addresstmp)//'training.data_1',status='replace',position='append')
-      write(Uout,*) "begin"
-      write(Uout,*) "comment   LiNiO2_633"
-      write(Uout,*) "lattice   5.7560070000E+00  0.0000000000E+00  0.0000000000E+00"
-      write(Uout,*) "lattice   0.0000000000E+00  4.9848490000E+00  0.0000000000E+00"
-      write(Uout,*) "lattice   0.0000000000E+00  0.0000000000E+00  1.4190003000E+01"
+    !open(newunit=Uout,file=trim(addresstmp)//'training.data_1',status='replace',position='append')
+    open(newunit=Uout,file=trim(addresstmp)//'training.data_1',status='replace')
+      !write(Uout,*) "begin"
+      !write(Uout,*) "comment   LiNiO2_633"
+      !write(Uout,*) "lattice   5.7560070000E+00  0.0000000000E+00  0.0000000000E+00"
+      !write(Uout,*) "lattice   0.0000000000E+00  4.9848490000E+00  0.0000000000E+00"
+      !write(Uout,*) "lattice   0.0000000000E+00  0.0000000000E+00  1.4190003000E+01"
+      do i = 1, 5
+        write(Uout,'(a)') trim(inp_train(i))
+      end do
       do i=1,Natom
         write(Uout,9998) &
          "atom", r(:,i,imode)*AUtoAng, alabel(i), 0.0d0, 0.0d0, 0.0d0, 0.0d0, 0.0d0, 0.0d0
       enddo
-      write(Uout,*) "energy   -2.0683566596E+02"
-      write(Uout,*) "charge    0.0000000000E+00"
-      write(Uout,*) "end"
+      do i = 6, 8
+        write(Uout,'(a)') trim(inp_train(i))
+      end do
+      !write(Uout,*) "energy   -2.0683566596E+02"
+      !write(Uout,*) "charge    0.0000000000E+00"
+      !write(Uout,*) "end"
     close(Uout)
 
-    call system('cd '//trim(addresstmp)//' ; mpiexec.hydra -n 1 ./n2training ; cd ../.. ')
+    call system('cd '//trim(addresstmp)//' ; ./n2training > /dev/null ; cd ../.. ')
+    !call system('cd '//trim(addresstmp)//' ; mpiexec.hydra -n 1 ./n2training ; cd ../.. ')
 
     open(newunit=Uinp,file=trim(addresstmp)//'foce_npp.out',status='old')
       read(Uinp,*) Eenergy(imode)
       do i = 1, Natom
         read(Uinp,*) dummyC, fr(:,i,imode)
-        !read(Uinp,*) dummyC, fx(i,imode), fy(i,imode), fz(i,imode)
       end do
     close(Uinp)
 
     fr(:,:,imode)=fr(:,:,imode)*eVAng_HartBohr*dp_inv
     Eenergy(imode) = Eenergy(imode) * eVtoAU
   end do
-
 
 return
 9999 format(3F24.16)
