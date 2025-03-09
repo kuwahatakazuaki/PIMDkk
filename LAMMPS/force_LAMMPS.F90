37586749927bc1129f63574a1f0e2cbb7df47b28
kuwahatakazuaki <kuwahata0615@gmail.com>
2025-03-09 10:27:22 +0900

    Add lattice in "force_LAMMPS.F90"

--- LAMMPS/force_LAMMPS.F90
+++ LAMMPS/force_LAMMPS.F90
@@ -1,5 +1,6 @@
 subroutine force_LAMMPS
   use Parameters
+  use utility
   USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_int64_t
   USE Struct_,           ONLY: xyz_, abc_, force_
   USE LAMMPSCalculator_, ONLY: lammps_calculator_, LAMMPSCalculator
@@ -10,7 +11,14 @@ subroutine force_LAMMPS
   integer :: i, Uout
   INTEGER(KIND=c_int64_t), POINTER :: total_atoms
   integer, allocatable :: lammps_id(:)
-  real(8) :: r_temp(3,Natom)
+  real(8) :: r_temp(3,Natom), lattice(3,3), lat_inv(3,3)
+  real(8), allocatable :: s(:,:)
+
+
+lattice(1,:) = [12.8512848674,    0.0000000000,    0.0000000000]
+lattice(2,:) = [ 0.2132032072,   12.6594123484,    0.0000000000]
+lattice(3,:) = [ 0.1438686986,   -0.0357082118,   12.7223433492]
+call get_inv_mat(lattice,lat_inv,3)
 
 ! Set LAMMPS calculator
   calculator = LAMMPSCalculator( lammps_file = 'lammps.in' )
@@ -18,9 +26,10 @@ subroutine force_LAMMPS
 !! Total number of atoms from LAMMPS
 !  total_atoms = calculator%lammps%extract_global( 'natoms' )
 
-allocate( cartesian_coordinates(Natom) )
-allocate( forces(Natom) )
-allocate( lammps_id(Natom) )
+  allocate( cartesian_coordinates(Natom) )
+  allocate( forces(Natom) )
+  allocate( lammps_id(Natom) )
+allocate( s(3,Natom) )
 
 ! Calculation in LAMMPS
   !do Imode = 1, Nbead
@@ -28,6 +37,9 @@ allocate( lammps_id(Natom) )
     r_temp(:,:) = r(:,:,Imode)
     r_temp(:,:) = r_temp(:,:) * AUtoAng
     do Iatom = 1, Natom
+s(:,Iatom) = matmul(r_temp(:,Iatom),lat_inv)
+s(:,Iatom) = s(:,Iatom) - dble(nint(s(:,Iatom)+0.5d0))+1.0d0
+r_temp(:,Iatom) = matmul(s(:,Iatom),lattice)
       cartesian_coordinates(Iatom)%x = r_temp(1,Iatom)
       cartesian_coordinates(Iatom)%y = r_temp(2,Iatom)
       cartesian_coordinates(Iatom)%z = r_temp(3,Iatom)
@@ -62,6 +74,23 @@ allocate( lammps_id(Natom) )
   CALL calculator%close()
   DEALLOCATE( cartesian_coordinates )
   DEALLOCATE( forces )
+
+contains
+
+  subroutine get_inv_mat(mat,inv,n)
+    integer :: n
+    real(8), intent(in)  :: mat(n,n)
+    real(8), intent(out) :: inv(n,n)
+    integer :: lwork, lda, info
+    real(8), allocatable :: work(:)
+    integer, allocatable :: ipiv(:)
+    inv(:,:) = mat(:,:)
+    lda = n
+    lwork = 64*n
+    allocate(work(lwork),ipiv(n))
+    call dgetrf(N, N, inv, lda, ipiv, info)
+    call dgetri(N, inv, lda, ipiv, work, lwork, info)
+  end subroutine get_inv_mat
 
 end subroutine force_LAMMPS
 
